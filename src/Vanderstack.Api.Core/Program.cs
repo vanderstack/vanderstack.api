310a19f46b3475b6afbd4f829163b130ca0c08f5
Zachary Vanderlaan <zvanderlaan@rad.theabr.org>
2017-07-03 00:16:16 -0700

    Initial Application with publish to Azure and Application Insights configured. Endpoints library is extending Core, as would be done when an end user is consuming the core Api. Endpoints library does not yet have any endpoints. This will come in a future feature after dependency injection is stubbed in.

---
+++ src/Vanderstack.Api.Core/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Vanderstack.Api.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseApplicationInsights()
+                .ConfigureServices((services) =>
+                    services.AddMvc()
+                )
+                //.ConfigureLogging((loggerFactory) => {
+                //    loggerFactory.AddConsole(configuration.GetSection("Logging"));
+                //    loggerFactory.AddDebug();
+                //})
+                .Configure((applicationBuilder) =>
+                    applicationBuilder.UseMvc()
+                )
+                .Build();
+
+            var environment = host.Services.GetService<IHostingEnvironment>();
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            IConfiguration configuration = configurationBuilder.Build();
+
+
+            host.Run();
+        }
+    }
+}
